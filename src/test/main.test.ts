f6e3b53e67a6a881f20324872d5d879c1bc0f139
Gabriel Grinberg <gabi.grinberg@gmail.com>
2023-04-24 20:06:27 +0300

    smoke test (#1)
    * smoke test
    
    * bob

---
+++ src/test/main.test.ts
@@ -0,0 +1,24 @@
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from "vscode";
+import * as path from "path";
+import { inspectLastWebviewElements } from "./testUtils";
+import assert = require("assert");
+
+suite("Extension Test Suite", () => {
+  test("Loads test flow and renders instance views", async () => {
+    const testFile = vscode.Uri.file(
+      path.resolve(__dirname, "../../test-fixtures/HelloWorld.flyde")
+    );
+
+    await vscode.commands.executeCommand(
+      "vscode.openWith",
+      testFile,
+      "flydeEditor"
+    );
+
+    const instances = await inspectLastWebviewElements(".ins-view-inner");
+
+    assert(instances.length === 4, "Expected fixture flow to have 4 instances");
+  });
+});
