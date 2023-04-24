f12e54939eccd183273b0b04a616c14ec827c056
Gabriel Grinberg <gabi.grinberg@gmail.com>
2023-04-24 20:05:52 +0300

    bob

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,8 +2,6 @@ import * as path from "path";
 
 import { runTests } from "@vscode/test-electron";
 
-process.env.MODE = "dev";
-
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
