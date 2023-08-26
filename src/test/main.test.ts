ac3fd848c57810a58e44aefb03ca2e6ee55a0bcf
Gabriel Grinberg <gabi.grinberg@gmail.com>
2023-08-26 07:00:13 +0300

    another attempt

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -7,6 +7,46 @@ import assert = require("assert");
 import { delay, eventually } from "@flyde/core";
 
 suite("Extension Test Suite", () => {
+  test("dummy test to check something", async () => {
+    console.log("Going to open a file");
+    const testFile = vscode.Uri.file(
+      path.resolve(__dirname, "../../test-fixtures/HelloWorld.flyde")
+    );
+
+    console.log("Opening file", testFile);
+
+    await vscode.commands.executeCommand(
+      "vscode.openWith",
+      testFile,
+      "flydeEditor"
+    );
+
+    console.log("Opened file");
+  }).timeout(10000);
+  test("Loads test flow and renders instance views", async () => {
+    const testFile = vscode.Uri.file(
+      path.resolve(__dirname, "../../test-fixtures/HelloWorld.flyde")
+    );
+
+    await vscode.commands.executeCommand(
+      "vscode.openWith",
+      testFile,
+      "flydeEditor"
+    );
+
+    await eventually(async () => {
+      const instances = await webviewTestingCommand("$$", {
+        selector: ".ins-view-inner",
+      });
+      console.log({ instances });
+
+      assert(
+        instances.length === 4,
+        "Expected fixture flow to have 4 instances"
+      );
+    }, 4000);
+  }).timeout(5000);
+
   test("Renders add node modal", async () => {
     const testFile = vscode.Uri.file(
       path.resolve(__dirname, "../../test-fixtures/HelloWorld.flyde")
@@ -39,29 +79,5 @@ suite("Extension Test Suite", () => {
       });
       assert(elements.length > 100, "Expected to find 100+ items in the menu");
     });
-  }).timeout(10000);
-
-  test("Loads test flow and renders instance views", async () => {
-    const testFile = vscode.Uri.file(
-      path.resolve(__dirname, "../../test-fixtures/HelloWorld.flyde")
-    );
-
-    await vscode.commands.executeCommand(
-      "vscode.openWith",
-      testFile,
-      "flydeEditor"
-    );
-
-    await eventually(async () => {
-      const instances = await webviewTestingCommand("$$", {
-        selector: ".ins-view-inner",
-      });
-      console.log({ instances });
-
-      assert(
-        instances.length === 4,
-        "Expected fixture flow to have 4 instances"
-      );
-    }, 4000);
-  }).timeout(10000);
+  }).timeout(5000);
 });
